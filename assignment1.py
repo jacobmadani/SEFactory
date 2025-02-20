0719dba83a2fe5748571f2d4c178bed0abd5bfa1
Yaacoub Elmadani <itsmadanii@gmail.com>
2025-02-20 19:10:57 +0200

    add some scripts to make it easier to read

--- assignment1.py
+++ assignment1.py
@@ -9,25 +9,31 @@ def salary_calculator(salary,savingsper,rentper,electricityper,value):
     electricity=salary*(electricityper/100)
     salary+=value
     savings=salary*(savingsper/100)
-
+    print("\n")
     print("The ammount allocated to savings is "+str(savings)+"$")
     print("The ammount allocated to rent is "+str(rent)+"$")
     print("The ammount allocated to electricity is "+str(electricity)+"$")
+    print("\n")
 
     total=savings+rent+electricity
     print("The total amount spent is: "+str(total)+"$")
 
     rem=salary-total
     print("The remainder of your salary is: "+str(rem)+"$")
+    print("\n")
 
     yrent=rent*12
     yelectricity=electricity*12
     print("Your estimate yearly rent cost is: "+str(yrent)+"$")
     print("Your estimate yearly electricity cost is: "+str(yelectricity)+"$")
+    print("\n")
 
     fun=pow(salary,2)
     print("Your total salary for the month raised to the power of 2 is: "+str(fun)+"$")
+msg="Here is your finances for "+str(month)
+print(msg.center(50)) 
 salary_calculator(salary,savingsper,rentper,electricityper,0) 
+print("\n")
 random=input("Would you like to add additional amount?(yes/no): ")
 if random=="yes":
       value=float(input("How much additional amount would u like to add?"))
