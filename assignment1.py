5dd316a245e41c2bb7ede65ee80275565e2582dc
Yaacoub Elmadani <itsmadanii@gmail.com>
2025-02-20 18:17:33 +0200

    put everything in a function for the if statement

--- assignment1.py
+++ assignment1.py
@@ -1,28 +1,36 @@
-salary=float(input("Please enter your salary: "))
-month=input("Please enter the name of the month: ")
-print("Please enter the percentages for each:")
-savingsper=float(input("Savings(%): "))
-rentper=float(input("Rent(%): "))
-electricityper=float(input("Electricity(%): "))
+def salary_calculator():
+    salary=float(input("Please enter your salary: "))
+    month=input("Please enter the name of the month: ")
+    print("Please enter the percentages for each:")
+    savingsper=float(input("Savings(%): "))
+    rentper=float(input("Rent(%): "))
+    electricityper=float(input("Electricity(%): "))
 
-savings=salary*(savingsper/100)
-rent=salary*(rentper/100)
-electricity=salary*(electricityper/100)
+    savings=salary*(savingsper/100)
+    rent=salary*(rentper/100)
+    electricity=salary*(electricityper/100)
 
-print("The ammount allocated to savings is "+str(savings)+"$")
-print("The ammount allocated to rent is "+str(rent)+"$")
-print("The ammount allocated to electricity is "+str(electricity)+"$")
+    print("The ammount allocated to savings is "+str(savings)+"$")
+    print("The ammount allocated to rent is "+str(rent)+"$")
+    print("The ammount allocated to electricity is "+str(electricity)+"$")
 
-total=savings+rent+electricity
-print("The total amount spent is: "+str(total)+"$")
+    total=savings+rent+electricity
+    print("The total amount spent is: "+str(total)+"$")
 
-rem=salary-total
-print("The remainder of your salary is: "+str(rem)+"$")
+    rem=salary-total
+    print("The remainder of your salary is: "+str(rem)+"$")
 
-yrent=rent*12
-yelectricity=electricity*12
-print("Your estimate yearly rent cost is: "+str(yrent)+"$")
-print("Your estimate yearly electricity cost is: "+str(yelectricity)+"$")
+    yrent=rent*12
+    yelectricity=electricity*12
+    print("Your estimate yearly rent cost is: "+str(yrent)+"$")
+    print("Your estimate yearly electricity cost is: "+str(yelectricity)+"$")
 
-fun=pow(salary,2)
-print("Your total salary for the month raised to the power of 2 is: "+str(fun)+"$")
+    fun=pow(salary,2)
+    print("Your total salary for the month raised to the power of 2 is: "+str(fun)+"$")
+salary_calculator() 
+random=input("Would you like to add additional amount?(yes/no): ")
+if random=="yes":
+      randad=float(input("How much additional amount would u like to add?"))
+else:
+    print("See you again!")
+
