0643eef1715f888149a05f51e0c4903067f361bb
Yaacoub Elmadani <itsmadanii@gmail.com>
2025-02-20 18:54:05 +0200

    connecting the function to the if statement to be able to add it to the salary only

--- assignment1.py
+++ assignment1.py
@@ -5,10 +5,10 @@ savingsper=float(input("Savings(%): "))
 rentper=float(input("Rent(%): "))
 electricityper=float(input("Electricity(%): "))
 def salary_calculator(salary,savingsper,rentper,electricityper,value):
-    salary+=value
-    savings=salary*(savingsper/100)
     rent=salary*(rentper/100)
     electricity=salary*(electricityper/100)
+    salary+=value
+    savings=salary*(savingsper/100)
 
     print("The ammount allocated to savings is "+str(savings)+"$")
     print("The ammount allocated to rent is "+str(rent)+"$")
@@ -31,7 +31,8 @@ salary_calculator(salary,savingsper,rentper,electricityper,0)
 random=input("Would you like to add additional amount?(yes/no): ")
 if random=="yes":
       value=float(input("How much additional amount would u like to add?"))
-      salary_calculator(value)
+      print("The amounts allocated after adding the new value will be as follow: ")
+      salary_calculator(salary,savingsper,rentper,electricityper,value) 
 else:
     print("See you again!")
 
